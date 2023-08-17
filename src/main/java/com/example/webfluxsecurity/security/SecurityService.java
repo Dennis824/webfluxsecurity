3881b22690c99d108b4c55841cd0876631567db4
Dennis <57582915+Dennis824@users.noreply.github.com>
2023-08-17 16:50:43 -0600

    feat (exception) added custom exception

--- src/main/java/com/example/webfluxsecurity/security/SecurityService.java
+++ src/main/java/com/example/webfluxsecurity/security/SecurityService.java
@@ -1,6 +1,7 @@
 package com.example.webfluxsecurity.security;
 
 import com.example.webfluxsecurity.entity.UserEntity;
+import com.example.webfluxsecurity.exception.AuthException;
 import com.example.webfluxsecurity.repository.UserRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -65,17 +66,16 @@ public class SecurityService {
         return userRepository.findByUsername(username)
                 .flatMap(user -> {
                     if (!user.isEnabled()) {
-                        return Mono.error(new RuntimeException(""));
-                    }
+                        return Mono.error(new AuthException("Account disabled", "USER_ACCOUNT_DISABLED"));                    }
 
                     if (!passwordEncoder.matches(password, user.getPassword())) {
-                        return Mono.error(new RuntimeException(""));
+                        return Mono.error(new AuthException("Invalid password", "INVALID_PASSWORD"));
                     }
 
                     return Mono.just(generateToken(user).toBuilder()
                             .userId(user.getId())
                             .build());
                 })
-                .switchIfEmpty(Mono.error(new RuntimeException("")));
+                .switchIfEmpty(Mono.error(new AuthException("Invalid username", "INVALID_USERNAME")));
     }
 }
